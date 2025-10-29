185efe5862050cc8dfb2e0bd90f3762357f9fec3
PiotrEliks <piotr.eliks@wp.pl>
2025-10-29 13:00:34 +0100

    refactor(backend)!: migrate backend from JavaScript to TypeScript

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,9 +11,10 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-export default app;
-const PORT = process.env.PORT || 5000;
+const PORT = Number(process.env.PORT) || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+export default app;
